0b28d1d0dce4321d7cd110f0c6802386e82b305f
samanthastaheli <sstaheli27@gmail.com>
2026-02-24 12:43:37 -0700

    build

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main() {
+
+    // Image
+
+    int image_width = 40;
+    int image_height = 40;
+
+    // Render
+
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = 0; j < image_height; j++) {
+        for (int i = 0; i < image_width; i++) {
+            //auto r = double(i) / (image_width - 1);
+            //auto g = double(j) / (image_height - 1);
+            //auto b = 0.0;
+
+            //int ir = int(255.999 * r);
+            //int ig = int(255.999 * g);
+            //int ib = int(255.999 * b);
+            int ir = int(255);
+            int ig = int(192);
+            int ib = int(203);
+
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+}
