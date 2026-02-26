f00c2ce76ca78d51f8ffcb406b33c6580a65951a
Samantha Staheli <sstaheli27@gmail.com>
2026-02-26 11:45:38 -0700

    made sphere

--- main.cc
+++ main.cc
@@ -6,7 +6,22 @@
 #include "ray.h"
 
 
+// Functions to create colors and shapes
+
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b * b - 4 * a * c;
+    return (discriminant >= 0);
+}
+
+
 color ray_color(const ray& r) {
+    if (hit_sphere(point3(0, 0, -1), 0.5, r))
+        //return color(255, 192, 203); // light pink color
+        return color(1, 0, 0);
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
@@ -67,11 +82,7 @@ int main() {
             color pixel_color = ray_color(r);            
             write_color(imageOut, pixel_color);
            
-            // light pink color
-
-            //int ir = int(255);
-            //int ig = int(192);
-            //int ib = int(203);
+            
 
             //vec3 value;
 			// imageOut << value.x << ' ' << value.y << ' ' << value.z << '\n';
