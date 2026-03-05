31b8bfb19bc5f3a953da5ff673d4fd80924f5c67
Samantha Staheli <sstaheli27@gmail.com>
2026-03-05 12:44:26 -0700

    added poly header file

---
+++ polygon.h
@@ -0,0 +1,91 @@
+#ifndef POLYGON_H
+#define POLYGON_H
+
+#include "vec3.h"
+
+color BackgroundColor = color(0.2, 0.2, 0.2);
+
+class polygon {
+private:
+    point3 center;
+    double radius;
+
+    double Kd;
+    double Ks;
+    double Ka;
+    double Kgls;
+    color Od;
+    color Os;
+public:
+    polygon(const point3& center, double radius,
+        double Kd, double Ks, double Ka, double Kgls,
+        color Od, color Os)
+        : center(center),
+        radius(std::fmax(0, radius)),
+        Kd(Kd),
+        Ks(Ks),
+        Ka(Ka),
+        Kgls(Kgls),
+        Od(Od),
+        Os(Os) {
+    }
+    auto hit(const ray& r) {
+        // h = d * (C - Q)
+        vec3 oc = center - r.origin();
+        auto a = r.direction().length_squared();
+        auto h = dot(r.direction(), oc);
+        auto c = oc.length_squared() - radius * radius;
+        auto discriminant = h * h - a * c;
+    
+        if (discriminant < 0) {
+            return -1.0;
+        }
+        else {
+		    // h - sqrt(h^2 - ac) / a
+            return (h - std::sqrt(discriminant)) / a;
+        }
+    }
+
+    color get_color(const ray& r) {
+        /*
+        slide 12 and 13 in IlluminationShading
+        Equations for I and R:
+            I = ambientTerm + diffuseTerm + specularTerm
+            ambientTerm = k_a * Ia * O_d
+            diffuseTerm = k_d * I_p * O_d * max(0, N
+            specularTerm = k_s * I_p * O_s * max(0, R dot O_s)^n
+
+            R = reflect direction 2N(N dot L) - L
+
+            Specular light = Os
+        */
+
+        auto t = hit(r);
+
+        if (t <= 0.0) {
+            return BackgroundColor;
+        }
+
+        // Variables 
+
+        vec3 N = unit_vector(r.at(t) - center);
+        vec3 L = unit_vector(vec3(0.0, 3.0, 2.0) - r.at(t)); // DirectionToLight
+        color Ia(0.0, 0.0, 0.0); // AmbientLight
+        color Ip(1.0, 1.0, 1.0); // LightColor
+
+        // Calaculate refection direction R
+
+        vec3 R = 2 * N * dot(N, L) - L; // reflect direction
+
+        // Calculate illumination I
+
+        color ambientTerm = Ka * Ia * Od;
+        color diffuseTerm = Kd * Ip * Od * std::fmax(0.0, dot(N, L));
+        color specularTerm = Ks * Ip * Os * std::pow(std::fmax(0.0, dot(R, L)), Kgls);
+        color I = ambientTerm + diffuseTerm + specularTerm;
+
+        return I;
+    }
+};
+
+#endif
